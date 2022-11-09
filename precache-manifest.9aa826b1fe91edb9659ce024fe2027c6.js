a19196785707632d2efc0f8087a518278f6b3cec
Hansal Scripts <hansal2022@gmail.com>
2022-11-09 19:58:48 +0200

    Updates

---
+++ precache-manifest.9aa826b1fe91edb9659ce024fe2027c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91d2c021a3a535195c2270973920482",
+    "url": "/seoclerks-affiliate-system/index.html"
+  },
+  {
+    "revision": "45a4f50b8ebb47908672",
+    "url": "/seoclerks-affiliate-system/static/css/2.518b6614.chunk.css"
+  },
+  {
+    "revision": "9aab196ddb50463b946b",
+    "url": "/seoclerks-affiliate-system/static/css/main.32115ad9.chunk.css"
+  },
+  {
+    "revision": "45a4f50b8ebb47908672",
+    "url": "/seoclerks-affiliate-system/static/js/2.648ed8c7.chunk.js"
+  },
+  {
+    "revision": "9aab196ddb50463b946b",
+    "url": "/seoclerks-affiliate-system/static/js/main.86b6d8a4.chunk.js"
+  },
+  {
+    "revision": "99cb1b9b86cc1d7e8f24",
+    "url": "/seoclerks-affiliate-system/static/js/runtime~main.8249b934.js"
+  }
+]);
